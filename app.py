01bef4c59d95e13a4adc68f66470c0d1330049a1
jayiuk <jayiuk987@gmail.com>
2024-06-11 04:51:30 +0900

    싹 다 수정

--- app.py
+++ app.py
@@ -6,13 +6,11 @@ import xgboost as xgb
 
 
     
-def new_data(goal, assist, foul, yellow, red, apperance, change, prob):
-    with open('xgb_tier.model', 'rb') as f:
+def new_data(goal, assist, apperance, prob, g_prob, foul_p, off_p, y_p, r_p, c_p):
+    with open('km.model', 'rb') as f:
         model = pickle.load(f)
-    nw = pd.DataFrame([{'득점' : goal, '도움' : assist, '파울' : foul, '경고' : yellow, '퇴장' : red, '출장' : apperance, '교체' : change, '경기당 기록' : prob}])
-    nx = xgb.DMatrix(data = nw)    
-    prediction = model.predict(nx)
-    prediction = int(prediction)
+    nw = pd.DataFrame([{'득점' : goal, '도움' : assist, '출장' : apperance, '경기당 기록' : prob, '골 전환율' : g_prob, '파울P' : 80 - foul_p, '오프사이드P' : 80 - off_p, '경고P' : 80 - y_p, '퇴장P' : 80 - r_p, '교체P' : 80 - c_p}])
+    prediction = model.predict(nw)
     return prediction
     
     
@@ -21,20 +19,22 @@ def main():
     st.write('K리그 데이터 기반')
     goal = st.number_input('득점', min_value = 0)
     assist = st.number_input('도움', min_value = 0)
-    foul = st.number_input('파울', min_value = 0)
-    yellow = st.number_input('경고', min_value = 0)
-    red = st.number_input('퇴장', min_value = 0)
     apperance = st.number_input('출장', min_value = 0)
-    change = st.number_input('교체', min_value = 0)
     prob = st.number_input('경기당 기록', min_value = 0.0)
+    g_prob = st.number_input('골 전환율', min_value = 0.0)
+    foul_p = st.number_input('파울', min_value = 0)
+    off_p = st.number_input('오프사이드', min_value = 0)
+    y_p = st.number_input('경고', min_value = 0)
+    r_p = st.number_input('퇴장', min_value = 0)
+    c_p = st.number_input('교체', min_value = 0)
     
-    result_list = ['S', 'D', 'C+', 'B', 'D+', 'C', 'A', 'A+', 'F', 'B+']
+    result_list = ['C', 'B', 'F', 'A', 'D']
     
     if st.button('분류 시작'):
-        r = new_data(goal, assist, foul, yellow, red, apperance, change, prob)
-        for i in range(0, 10):
+        r = new_data(goal, assist, apperance, prob, g_prob, foul_p, off_p, y_p, r_p, c_p)
+        for i in range(0, 5):
             if r == i:
-                result = result_list[r]
+                result = result_list[i]
         st.success(f'티어 : {result}')
 
 if __name__ == "__main__":
--- k_league_tier_fw.py
+++ k_league_tier_fw.py
@@ -1,16 +1,21 @@
 import pandas as pd
 from sklearn.cluster import KMeans
+import pickle
 
 import os
 path = 'C:/k_league_tier'
 data = pd.read_csv(os.path.join(path, 'total_ranking.csv'))
 
-km = KMeans(n_clusters = 10)
+km = KMeans(n_clusters = 5)
 km.fit(data)
 pred = km.predict(data)
 
-    
+
 c_data = data.copy()
 c_data['target'] = pred
 c_data
 c_data.to_csv('tier.csv', index = False)
+
+
+name = 'km.model'
+pickle.dump(km, open(name, 'wb'))
